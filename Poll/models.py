42cf6c0fcdb745aa26177175a6add5450c71b9f5
JaikoSuno <juan.quispe06@gmail.com>
2019-07-30 20:06:36 -0400

    install database

--- Poll/models.py
+++ Poll/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tabla_usuario(models.Model):
+    user = models.CharField(max_length=200)
+    pass_user = models.CharField(max_length=200)
--- ProyectoGuiarConsultores/settings.py
+++ ProyectoGuiarConsultores/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ProyectoGuiarConsultores.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guiarconsultores',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
