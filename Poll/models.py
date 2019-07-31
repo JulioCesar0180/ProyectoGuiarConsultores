b2452bcd5f65246d7aefa0703b76c137118b1a91
Pablo Julio <pablo.julio.vit@gmail.com>
2019-07-31 18:46:37 -0400

    Update: Campos Tabla Usuarios
    Se agregan todos los campos solicitados por el cliente al perfil del usuario dentro del sistema

--- Poll/models.py
+++ Poll/models.py
@@ -8,4 +8,11 @@ class Tabla_usuario(models.Model):
     pass_user = models.CharField(max_length=200)
     nombre_empresa = models.CharField(max_length=100, default="")
     rut_empresa = models.CharField(max_length=15, default="")
-
+    direccion_empresa = models.CharField(max_length=100, default="")
+    experiencia_empresa = models.CharField(max_length=10, default="")
+    ciudad_empresa = models.CharField(max_length=20, default="")
+    comuna_empresa = models.CharField(max_length=20, default="")
+    nombre_contacto_empresa = models.CharField(max_length=100, default="")
+    telefono_empresa = models.CharField(max_length=20, default="")
+    email_empresa = models.CharField(max_length=100, default="")
+    ventas_anuales_empresa = models.CharField(max_length=50, default="")
