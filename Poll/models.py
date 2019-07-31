7026df404690cf61a1ccc212f7a3084bd8373ddf
JaikoSuno <juan.quispe06@gmail.com>
2019-07-31 17:43:15 -0400

    Modified Tabla_usuario

--- Poll/models.py
+++ Poll/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Tabla_usuario(models.Model):
     user = models.CharField(max_length=200)
     pass_user = models.CharField(max_length=200)
+    nombre_empresa = models.CharField(max_length=100, default="")
+    rut_empresa = models.CharField(max_length=15, default="")
+
