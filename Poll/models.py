fdd74091e229b97d5a915a9c1f1ae76a2970a25d
Pablo Julio <pablo.julio.vit@gmail.com>
2019-07-31 20:01:54 -0400

    ADD: Tablas adicionales a la Encuesta
    Se agregan las tablas correspondientes al resto de los modulos pertenecientes a la encuesta incluyendo los resultados finales y las recomendaciones correspondientes

--- Poll/models.py
+++ Poll/models.py
@@ -65,3 +65,92 @@ class Tabla_resultados_servicios(models.Model):
     answer12 = models.CharField(max_length=2, default="")
     answer13 = models.CharField(max_length=2, default="")
     answer14 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_dotacion(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+    answer6 = models.CharField(max_length=2, default="")
+    answer7 = models.CharField(max_length=2, default="")
+    answer8 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_gestion(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+    answer6 = models.CharField(max_length=2, default="")
+    answer7 = models.CharField(max_length=2, default="")
+    answer8 = models.CharField(max_length=2, default="")
+    answer9 = models.CharField(max_length=2, default="")
+    answer10 = models.CharField(max_length=2, default="")
+    answer11 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_procesos(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_explosivos(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_electricidad(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_sustancias_peligrosas(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+    answer5 = models.CharField(max_length=2, default="")
+    answer6 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_altura(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    answer1 = models.CharField(max_length=2, default="")
+    answer2 = models.CharField(max_length=2, default="")
+    answer3 = models.CharField(max_length=2, default="")
+    answer4 = models.CharField(max_length=2, default="")
+
+class Tabla_resultados_finales(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    riesgo_transporte = models.CharField(max_length=2, default="")
+    riesgo_construccion = models.CharField(max_length=2, default="")
+    riesgo_manufactura = models.CharField(max_length=2, default="")
+    riesgo_servicios = models.CharField(max_length=2, default="")
+    riesgo_dotacion = models.CharField(max_length=2, default="")
+    riesgo_gestion = models.CharField(max_length=2, default="")
+    riesgo_procesos = models.CharField(max_length=2, default="")
+    riesgo_explosivos = models.CharField(max_length=2, default="")
+    riesgo_electricidad = models.CharField(max_length=2, default="")
+    riesgo_sustancias_peligrosas = models.CharField(max_length=2, default="")
+    riesgo_altura = models.CharField(max_length=2, default="")
+
+class Tabla_priorizacion_riesgos(models.Model):
+    empresa = models.ForeignKey(Tabla_usuario, on_delete=models.CASCADE)
+    resultados = models.ForeignKey(Tabla_resultados_finales, on_delete=models.CASCADE)
+    responsabilidad_civil_empresa = models.CharField(max_length=2, default="")
+    equipos_moviles = models.CharField(max_length=2, default="")
+    transporte_terrestre = models.CharField(max_length=2, default="")
+    vehiculos_comerciales_livianos = models.CharField(max_length=2, default="")
+    vehiculos_comerciales_pesados = models.CharField(max_length=2, default="")
+    accidentes_personales = models.CharField(max_length=2, default="")
