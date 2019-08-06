5f0149feee1488afe83c3ad5c6a2f6376db24575
Manuel <manuelzuletab@gmail.com>
2019-08-05 20:42:07 -0400

    Update: Agregada segunda página de encuesta
    Por arreglar: Llevar a la página anterior
    Por agregar: Preguntas de la segunda parte y la parte tres.

--- Poll/views.py
+++ Poll/views.py
@@ -6,5 +6,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "MideTuRiesgo/mideturiesgo.html")
 
+def dos(request):
+    return render(request, "MideTuRiesgo/mideturiesgo2.html")
+
 
 
