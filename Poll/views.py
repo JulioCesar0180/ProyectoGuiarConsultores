7606d9ba8fbc200b27771d6d549adde65bb73af5
Barbs <xbarbs.hp@gmail.com>
2019-08-06 20:15:25 -0400

    ADD: Base header

--- Poll/views.py
+++ Poll/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 
