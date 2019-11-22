5d831b6c249741f17c2e525785db992000464d42
Barbs <xbarbs.hp@gmail.com>
2019-11-22 17:23:41 -0300

    Update mensajes
    Se corrigió que todos los mensajes tuvieran su color correcto.

--- ProyectoGuiarConsultores/settings.py
+++ ProyectoGuiarConsultores/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +156,8 @@ EMAIL_USE_TLS = True
 
 AUTH_USER_MODEL = "Poll.UserGuiar"
 
+MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
+                message_constants.INFO: 'info',
+                message_constants.SUCCESS: 'success',
+                message_constants.WARNING: 'warning',
+                message_constants.ERROR: 'danger',}
