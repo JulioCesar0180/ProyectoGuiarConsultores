f959e7bca668e243fddcc30e52385c0108d77a92
JaikoSuno <juan.quispe06@gmail.com>
2019-12-13 13:49:53 -0300

    Update Page 4 - Fields BD

--- Poll/admin.py
+++ Poll/admin.py
@@ -48,6 +48,7 @@ admin.site.register(TablaResultadosTiempoPreven)
 
 admin.site.register(TablaManiExplosivos)
 admin.site.register(TablaResultadosManiExplosivos)
+admin.site.register(TablaElectricidad)
 admin.site.register(TablaResultadoElectricidad)
 
 admin.site.register(TablaPrioridadBase)
