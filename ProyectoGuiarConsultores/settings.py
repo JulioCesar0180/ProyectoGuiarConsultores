9ef466a9d4de1203c52691b2c44c67b623bfc337
JaikoSuno <juan.quispe06@gmail.com>
2019-12-20 18:16:01 -0300

    Update File Database

--- ProyectoGuiarConsultores/settings.py
+++ ProyectoGuiarConsultores/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'ProyectoGuiarConsultores.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -85,6 +86,14 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
     },
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
